8b50d93a18bcd75e213ca11c2c6bba3ec7d5caa0
tanishqsujan <tanishq.kothamasu@gmail.com>
2025-06-14 11:05:58 +0400

    Slicing

---
+++ slice.py
@@ -0,0 +1,12 @@
+inp= 'CODINGAL'
+
+#string slicing using indexing sequence
+print(inp[:3])
+print(inp[3:])
+print(inp[1:5:2])
+
+#print the string in reverse order using string
+
+print(inp[::-1])
+print(inp[-1:-9:-2])
+print(inp[-2:-9:-2])
